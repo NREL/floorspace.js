d1e5ade9ac2eaeb375e8b6afa3a91c8b7854f0d5
Katie Noland <katie@devetry.com>
2016-12-29 01:36:25 -0700

    a bit of vuex boilerplate and a rough draft of a potential schema for front end use

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+const store = new Vuex.Store({
+    state: {
+        'stories ': [{
+            'thermal-zones': [{
+                'spaces': [{
+                    'id': null,
+                    'surfaces': [{
+                        'point1': {
+                            'x': null,
+                            'y': null
+                        },
+                        'point2': {
+                            'x': null,
+                            'y': null
+                        },
+                        'components': [
+                            'this needs to be investigated further'
+                        ],
+                        'space-left': {
+                            'id': null
+                        },
+                        'space-right': {
+                            'id': null
+                        }
+
+                    }]
+                }]
+            }]
+        }]
+    },
+    mutations: {
+    }
+})
