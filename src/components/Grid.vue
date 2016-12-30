b575993594d3e450dad7ed79625b8c626db00616
Katie Noland <katie@devetry.com>
2016-12-30 15:54:20 -0700

    Finally got snappy grid working

---
+++ src/components/Grid.vue
@@ -0,0 +1,163 @@
+<template>
+    <div id="canvas">
+        <svg ref="grid" @click="addPoint"></svg>
+    </div>
+</template>
+
+<script>
+var d3 = require('d3');
+export default {
+    name: 'canvas',
+    data: function() {
+        return {
+            gridResolution: 20,
+            // array of spaces drawn - going to be synced with navigation and displayed
+            spaces: [],
+            // space being drawn
+            points: []
+        };
+    },
+    mounted: function() {
+        // draw the grid
+        let svg = d3.select('#canvas svg');
+
+        svg.selectAll('.vertical')
+            .data(d3.range(1, this.$refs.grid.clientWidth / this.gridResolution))
+            .enter().append('line')
+            .attr('class', 'vertical')
+            .attr('x1', (d) => { return d * this.gridResolution; })
+            .attr('y1', 0)
+            .attr('x2', (d) => { return d * this.gridResolution; })
+            .attr('y2', this.$refs.grid.clientHeight);
+
+        svg.selectAll('.horizontal')
+            .data(d3.range(1, this.$refs.grid.clientHeight / this.gridResolution))
+            .enter().append('line')
+            .attr('class', 'horizontal')
+            .attr('x1', 0)
+            .attr('y1', (d) => { return d * this.gridResolution; })
+            .attr('x2', this.$refs.grid.clientWidth)
+            .attr('y2', (d) => { return d * this.gridResolution; });
+    },
+    watch: {
+        // when a space is added, draw all spaces
+        spaces: function() { this.drawPolygonSpaces(); },
+        // when a point is added, draw all points
+        points: function() { this.drawPoints(); }
+    },
+    methods: {
+        addPoint: function(e) {
+            let x = round(Math.max(2, Math.min(this.$refs.grid.clientWidth - 2, e.offsetX)), this.gridResolution),
+                y = round(Math.max(2, Math.min(this.$refs.grid.clientHeight - 2, e.offsetY)), this.gridResolution);
+
+            this.points.push({
+                x: x,
+                y: y
+            });
+
+            function round(p, n) {
+                return p % n < n / 2 ? p - (p % n) : p + n - (p % n);
+            }
+        },
+
+        drawPoints: function() {
+            // draw new points
+            d3.select('#canvas svg')
+                .selectAll('circle').data(this.points)
+                .enter().append('circle')
+                .attr('cx', (d, i) => {
+                    return d.x;
+                })
+                .attr('cy', (d, i) => {
+                    return d.y;
+                })
+                .attr('r', 2);
+
+            //connect the points with a guideline
+            this.drawPolygonEdges();
+
+            // set a click listener for the first point in the polynomial, when it is clicked close the shape
+            d3.select('#canvas svg').select('circle')
+                .on('click', () => {
+                    // create the shape - triggers this.drawPolygonSpaces()
+                    this.spaces.push({ points: this.points });
+                    // clear points, prevent a new point from being created by this click event
+                    d3.event.stopPropagation();
+                    this.points = [];
+                })
+                // styles for the first point in the polygon
+                .classed('origin', true)
+                .attr('r', 5);
+        },
+        drawPolygonEdges: function() {
+            // remove expired paths
+            d3.selectAll("#canvas path").remove();
+
+            var line = d3.line()
+                .x((d) => { return d.x; })
+                .y((d) => { return d.y; });
+
+            d3.select('#canvas svg').append("path")
+                .datum(this.points)
+                .attr("fill", "none")
+                .attr("stroke-width", "1")
+                .attr("d", line)
+                // prevent overlapping the points - screws up click events
+                .lower();
+                
+            // keep grid lines under polygon edges
+            d3.selectAll('.vertical, .horizontal').lower();
+        },
+        drawPolygonSpaces: function() {
+            // draw a polygon for each space
+            d3.select('#canvas svg').selectAll('polygon')
+                .data(this.spaces).enter()
+                .append('polygon')
+                .attr('points', (d, i) => {
+                    var pointsString = "";
+                    d.points.forEach((p) => {
+                        pointsString += (p.x + ',' + p.y + ' ');
+                    });
+                    return pointsString;
+                })
+                .attr("stroke-width", "1");
+
+            //remove expired points and guidelines
+            d3.selectAll("#canvas path").remove();
+            d3.selectAll('#canvas circle').remove();
+        },
+        drawRectSpaces: function(e) {
+            d3.select('#canvas svg').selectAll('rect')
+                .data(this.rectSpaces).enter()
+                .append('rect')
+                .attr('x', (d, i) => {
+                    return d.origin.x;
+                })
+                .attr('y', (d, i) => {
+                    return d.origin.y;
+                })
+                .attr('width', (d, i) => {
+                    return d.size.width;
+                })
+                .attr('height', (d, i) => {
+                    return d.size.height;
+                });
+        }
+    }
+}
+
+</script>
+<style lang="scss" scoped>
+@import "./../scss/config";
+// we can't style the dynamically created d3 elements here, put those styles in the scss folder
+#canvas {
+    background-color: $gray-darkest;
+    svg {
+        height: 100%;
+        width: 100%;
+        .horizontal, .vertical {
+            fill: gray;
+        }
+    }
+}
+</style>
