dd881e267256178386233b49eaa086ea636ccd32
Katie Noland <katie@devetry.com>
2017-01-01 16:56:44 -0700

    some data store changes, still very tentative

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 /* eslint-disable no-new */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
@@ -6,6 +5,7 @@ import Vue from 'vue'
 import store from './store/index.js'
 import App from './App'
 
+// mount the root vue instance
 new Vue({
     el: '#app',
     template: '<App/>',
--- src/store/state.js
+++ src/store/state.js
@@ -1,58 +1,38 @@
 export default {
-    'stories': [{
-        'name': null,
-        'id': null,
-        'thermalZoneIds': []
-    }],
-    'thermalZones': [{
-        'name': null,
-        'id': null,
-        'spaceIds': []
-    }],
-    'spaces': [{
-        'name': null,
-        'id': null,
-        'surfaceIds': []
-    }],
-    'surfaces': [{
-        'id': null,
-        'p1': null,
-        'p2': null
-    }],
-    'points': [{
-        'id': null,
-        'x': null,
-        'y': null
-    }]
-
+    'config': {
+        'language': null,
+        'units': null
+    },
+    'application': {
+        'scale': null,
+        'drawingMode': '',
+        'tool': ''
+    },
+    'geometry': {
+        'stories': [{
+            'name': null,
+            'id': null,
+            'thermalZoneIds': []
+        }],
+        'thermalZones': [{
+            'name': null,
+            'id': null,
+            'spaceIds': []
+        }],
+        'spaces': [{
+            'name': null,
+            'id': null,
+            'surfaceIds': []
+        }],
+        'surfaces': [{
+            'id': null,
+            'p1': null,
+            'p2': null
+        }],
+        'points': [{
+            'id': null,
+            'x': null,
+            'y': null
+        }]
+    }
 }
-
-// export default {
-//     'stories': [{
-//         'thermal-zones': [{
-//             'spaces': [{
-//                 'id': null,
-//                 'surfaces': [{
-//                     'point1': {
-//                         'x': null,
-//                         'y': null
-//                     },
-//                     'point2': {
-//                         'x': null,
-//                         'y': null
-//                     },
-//                     'components': [
-//                         'this needs to be investigated further'
-//                     ],
-//                     'space-left': {
-//                         'id': null
-//                     },
-//                     'space-right': {
-//                         'id': null
-//                     }
-//
-//                 }]
-//             }]
-//         }]
-//     }]
-// }
