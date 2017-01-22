3981518934e3e826e4479a8f35eab78e46822567
Katie Noland <katie@devetry.com>
2017-01-21 18:39:23 -0700

    trying to figure out how to test actions with no real context val

--- test/unit/specs/store/Geometry/Actions.spec.js
+++ test/unit/specs/store/Geometry/Actions.spec.js
@@ -1,45 +1,51 @@
-// import { expect } from 'chai'
-// import { geometry as Geometry, helpers } from '../../../../../src/store/modules/geometry/index.js'
-// import factory from '../../../../../src/store/factory/index.js'
-//
-// describe('mutations', () => {
-//     it('initGeometry', () => {
-//         // mock state
-//         const geometryState = [];
-//
-//         const story = new factory.Story();
-//         Geometry.actions.initGeometry(geometryState, {
-//             story: story
-//         });
-//
-//         // check that geometry was created and saved
-//         expect(geometryState.length).to.equal(1);
-//         expect(geometryState[0].vertices.length).to.equal(0);
-//         expect(geometryState[0].edges.length).to.equal(0);
-//         expect(geometryState[0].faces.length).to.equal(0);
-//
-//         // check that the story has a reference to the new geometry
-//         expect(story.geometry_id).to.be.ok;
-//     });
-// });
-//
-// Context(geometryState) {
-//     geometryState = geometryState || {};
-//     return {
-//         commit (type, payload, options) {
-//             if (options.root) {
-//                 // TODO: mock out root commits somehow
-//             } else {
-//                 Geometry.mutations[type](geometryState, payload);
-//             }
-//         },
-//         dispatch (type, payload) {
-//             debugger
-//             Geometry.actions[type](this, payload);
-//         },
-//         getters: geometry.getters
-//         rootGetters: {},
-//         rootState: {},
-//         state: geometryState
-//     };
-// }
+import { expect } from 'chai'
+import { geometry as Geometry, helpers } from '../../../../../src/store/modules/geometry/index.js'
+import factory from '../../../../../src/store/factory/index.js'
+
+describe('actions', () => {
+    it('initGeometry', () => {
+        testAction(Geometry.actions.initGeometry, {}, {}, [{
+            type: 'initGeometry',
+            testPayload (payload) {
+                expect(payload.geometry.id).to.be.a('number');
+                expect(payload.geometry.vertices).to.be.an('array');
+                expect(payload.geometry.edges).to.be.an('array');
+                expect(payload.geometry.faces).to.be.an('array');
+            }
+        }])
+    });
+});
+
+// arguments:
+// action - the actual action function
+// actionPayload - the payload object to call the action function with
+// state - the state of the store you want to test the action with
+// expectedMutations - the mutations that the action should trigger in order - these can contain a type and tests for their payloads
+const testAction = (action, actionPayload, state, expectedMutations) => {
+    var mutationCount = 0;
+    const context = {
+        commit (type, mutationPayload) {
+            // loop up the next expected mutation
+            const expectedMutation = expectedMutations[mutationCount];
+
+            // check for the correct type
+            expect(expectedMutation.type).to.equal(type);
+
+            // check for the correct payload
+            if (mutationPayload) {
+                expectedMutation.testPayload(mutationPayload);
+            }
+
+            mutationCount++;
+        },
+        state: state
+    };
+
+    // call the action with mocked store and arguments
+    action(context, actionPayload);
+
+    // check if no mutations should have been dispatched
+    if (expectedMutations.length === 0) {
+        expect(mutationCount).to.equal(0);
+    }
+}
