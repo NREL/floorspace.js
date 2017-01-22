14a23f9d3f8e41e30052f775b634b75996de3ce4
Katie Noland <katie@devetry.com>
2017-01-21 18:57:55 -0700

    helper for testing actions

--- test/unit/specs/store/Geometry/Actions.spec.js
+++ test/unit/specs/store/Geometry/Actions.spec.js
@@ -1,12 +1,18 @@
 import { expect } from 'chai'
-import { geometry as Geometry, helpers } from '../../../../../src/store/modules/geometry/index.js'
+import { geometry as Geometry } from '../../../../../src/store/modules/geometry/index.js'
 import factory from '../../../../../src/store/factory/index.js'
+import testAction from '../helpers/testAction.js'
 
 describe('actions', () => {
     it('initGeometry', () => {
-        testAction(Geometry.actions.initGeometry, {}, {}, [{
+        const story = new factory.Story();
+        testAction(Geometry.actions.initGeometry, {
+            story: story
+        }, {}, [{
             type: 'initGeometry',
             testPayload (payload) {
+                expect(payload.story).to.equal(story);
+
                 expect(payload.geometry.id).to.be.a('number');
                 expect(payload.geometry.vertices).to.be.an('array');
                 expect(payload.geometry.edges).to.be.an('array');
@@ -14,38 +20,11 @@ describe('actions', () => {
             }
         }])
     });
+
+    it('createFaceFromPoints', () => {
+        testAction(Geometry.actions.createFaceFromPoints, {}, {}, [{
+            type: 'createFace',
+            testPayload (payload) { }
+        }])
+    });
 });
-
-// arguments:
-// action - the actual action function
-// actionPayload - the payload object to call the action function with
-// state - the state of the store you want to test the action with
-// expectedMutations - the mutations that the action should trigger in order - these can contain a type and tests for their payloads
-const testAction = (action, actionPayload, state, expectedMutations) => {
-    var mutationCount = 0;
-    const context = {
-        commit (type, mutationPayload) {
-            // loop up the next expected mutation
-            const expectedMutation = expectedMutations[mutationCount];
-
-            // check for the correct type
-            expect(expectedMutation.type).to.equal(type);
-
-            // check for the correct payload
-            if (mutationPayload) {
-                expectedMutation.testPayload(mutationPayload);
-            }
-
-            mutationCount++;
-        },
-        state: state
-    };
-
-    // call the action with mocked store and arguments
-    action(context, actionPayload);
-
-    // check if no mutations should have been dispatched
-    if (expectedMutations.length === 0) {
-        expect(mutationCount).to.equal(0);
-    }
-}
---
+++ test/unit/specs/store/helpers/testAction.js
@@ -0,0 +1,33 @@
+// arguments:
+// action - the actual action function
+// actionPayload - the payload object to call the action function with
+// state - the state of the store you want to test the action with
+// expectedMutations - the mutations that the action should trigger in order - these can contain a type and tests for their payloads
+export default function testAction (action, actionPayload, state, expectedMutations) {
+    var mutationCount = 0;
+    const context = {
+        commit (type, mutationPayload) {
+            // loop up the next expected mutation
+            const expectedMutation = expectedMutations[mutationCount];
+
+            // check for the correct type
+            expect(expectedMutation.type).to.equal(type);
+
+            // check for the correct payload
+            if (mutationPayload) {
+                expectedMutation.testPayload(mutationPayload);
+            }
+
+            mutationCount++;
+        },
+        state: state
+    };
+
+    // call the action with mocked store and arguments
+    action(context, actionPayload);
+
+    // check if no mutations should have been dispatched
+    if (expectedMutations.length === 0) {
+        expect(mutationCount).to.equal(0);
+    }
+}
