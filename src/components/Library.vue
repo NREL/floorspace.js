a30983e1c01dfda516dbfb4844504bca785209c6
Katie Noland <katie@devetry.com>
2017-02-19 10:45:20 -0700

    support for arbitrary fields on objects in lib

--- src/components/Library.vue
+++ src/components/Library.vue
@@ -3,33 +3,46 @@ Redistribution and use in source and binary forms, with or without modification,
 (1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 (2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 (3) Neither the name of the copyright holder nor the names of any contributors may be used to endorse or promote products derived from this software without specific prior written permission from the respective party.
-(4) Other than as required in clauses (1) and (2), distributions in any form of modifications or other derivative works may not use the "OpenStudio" trademark, "OS", "os", or any other confusingly similar designation without specific prior written permission from Alliance for Sustainable Energy, LLC.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER, THE UNITED STATES GOVERNMENT, OR ANY CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. -->
+(4) Other than as required in clauses (1) and (2), distributions in any form of modifications or other derivative works may not use the 'OpenStudio' trademark, 'OS', 'os', or any other confusingly similar designation without specific prior written permission from Alliance for Sustainable Energy, LLC.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER, THE UNITED STATES GOVERNMENT, OR ANY CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. -->
 
 <template>
-<section id="library">
+<section id='library'>
     <header>
         <h1>Object Library</h1>
-        <button @click="createObject()" id="import">New Object</button>
+        <button @click='createObject()' id='import'>New Object</button>
+    </header>
 
-        <div class="input-select" id="typeSelect">
+    <form>
+        <div class='input-select' id='typeSelect'>
             <label>Type</label>
-            <select v-model="type">
-                <option v-for="(objects, type) in library">{{ displayTypeName(type) }}</option>
+            <select v-model='displayType'>
+                <option v-for='(objects, type) in library'>{{ displayTypeForType(type) }}</option>
             </select>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13 14" height="10px">
-                <path d="M.5 0v14l11-7-11-7z" transform="translate(13) rotate(90)"></path>
+            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 13 14' height='10px'>
+                <path d='M.5 0v14l11-7-11-7z' transform='translate(13) rotate(90)'></path>
             </svg>
         </div>
 
-    </header>
+        <div class='input-text'>
+            <label>name</label>
+            <input v-model='objectName'>
+        </div>
 
-    <div class="input-number">
-        <label>name</label>
-        <input v-model="objectName">
-    </div>
+        <div v-for='field in fields'>
+            <div class='input-text'>
+                <label>Label</label>
+                <input v-model='field.label'>
+
+                <label>Value</label>
+                <input v-model='field.value'>
+            </div>
+        </div>
+
+        <button @click.prevent='addField()' id='import'>Add Field</button>
+    </form>
     <ul>
-        <li v-for="object in objects">{{object}}</li>
+        <li v-for='object in objects'>{{object}}</li>
     </ul>
 
 </section>
@@ -41,65 +54,70 @@ import factory from './../store/modules/models/factory'
 
 const map = {
     building_units: {
-        displayName: "Building Unit",
-        constructor: factory.BuildingUnit
+        displayName: 'Building Unit',
+        create: factory.BuildingUnit
     },
     thermal_zones: {
-        displayName: "Thermal Zone",
-        constructor: factory.ThermalZone
+        displayName: 'Thermal Zone',
+        create: factory.ThermalZone
     },
     space_types: {
-        displayName: "Space Type",
-        constructor: factory.SpaceType
+        displayName: 'Space Type',
+        create: factory.SpaceType
     },
     construction_sets: {
-        displayName: "Construction Set",
-        constructor: factory.ConstructionSet
+        displayName: 'Construction Set',
+        create: factory.ConstructionSet
     },
     constructions: {
-        displayName: "Construction",
-        constructor: factory.Construction
+        displayName: 'Construction',
+        create: factory.Construction
     },
     windows: {
-        displayName: "Window",
-        constructor: factory.Window
+        displayName: 'Window',
+        create: factory.Window
     },
     daylighting_controls: {
-        displayName: "Daylighting Control",
-        constructor: factory.DaylightingControl
+        displayName: 'Daylighting Control',
+        create: factory.DaylightingControl
     }
 };
 
 export default {
-    name: 'navigation',
+    name: 'library',
     data() {
         return {
-            type: null,
-            objectName: null
+            displayType: null,
+            objectName: null,
+            fields: []
         };
     },
     mounted () {
-        this.type = this.displayTypeName(Object.keys(this.library)[0]);
+        this.displayType = this.displayTypeForType(Object.keys(this.library)[0]);
     },
     computed: {
         ...mapState({
             library: state => state.models.library
         }),
         objects () {
-            const type = Object.keys(map).find(k => map[k].displayName === this.type);
-
-            return this.library[type];
+            return this.library[this.typeForDisplayType(this.displayType)];
         }
     },
     methods: {
-        displayTypeName (type) {
-            return map[type].displayName;
+        displayTypeForType (type) { return map[type].displayName; },
+        typeForDisplayType (displayType) { return Object.keys(map).find(k => map[k].displayName === this.displayType); },
+        addField () {
+            this.fields.push({
+                label: '',
+                value: ''
+            });
         },
         createObject () {
-
-
-            const type = Object.keys(map).find(k => map[k].displayName === this.type)
-            var newObject = new map[type].constructor(this.objectName);
+            const type = this.typeForDisplayType(this.displayType);
+            var newObject = new map[type].create(this.objectName);
+            this.fields.forEach((field) => {
+                newObject[field.label] = field.value;
+            })
             this.$store.dispatch('models/createObjectWithType', {
                 type: type,
                 object: newObject
@@ -109,8 +127,8 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-@import "./../scss/config";
+<style lang='scss' scoped>
+@import './../scss/config';
 #library {
     background-color: $gray-medium-light;
     overflow: scroll;
@@ -127,6 +145,14 @@ export default {
             & > select {
                 width: 8.5rem;
             }
+        }
+    }
+    form {
+        .input-select select {
+            width: 10rem;
+        }
+        .input-text {
+            margin-top: 1rem;
         }
     }
 }
