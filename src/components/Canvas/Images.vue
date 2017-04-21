2a99e36aee405264d8f0ec7645e6e4e825077ceb
Katie Noland <katie@devetry.com>
2017-04-21 14:34:39 -0600

    more work on image mode

--- src/components/Canvas/Images.vue
+++ src/components/Canvas/Images.vue
@@ -24,86 +24,90 @@ export default {
         this.loadImages();
     },
     methods: {
+        update(activeAnchor) {
+            var group = activeAnchor.getParent();
+            var topLeft = group.get('.topLeft')[0];
+            var topRight = group.get('.topRight')[0];
+            var bottomRight = group.get('.bottomRight')[0];
+            var bottomLeft = group.get('.bottomLeft')[0];
+            var image = group.get('Image')[0];
+            var anchorX = activeAnchor.getX();
+            var anchorY = activeAnchor.getY();
+            // update anchor positions
+            switch (activeAnchor.getName()) {
+                case 'topLeft':
+                    topRight.setY(anchorY);
+                    bottomLeft.setX(anchorX);
+                    break;
+                case 'topRight':
+                    topLeft.setY(anchorY);
+                    bottomRight.setX(anchorX);
+                    break;
+                case 'bottomRight':
+                    bottomLeft.setY(anchorY);
+                    topRight.setX(anchorX);
+                    break;
+                case 'bottomLeft':
+                    bottomRight.setY(anchorY);
+                    topLeft.setX(anchorX);
+                    break;
+            }
+            image.position(topLeft.position());
+            var width = topRight.getX() - topLeft.getX();
+            var height = bottomLeft.getY() - topLeft.getY();
+            if (width && height) {
+                image.width(width);
+                image.height(height);
+            }
+        },
+        addAnchor(group, x, y, name) {
+            var stage = group.getStage();
+            var layer = group.getLayer();
+            var anchor = new Konva.Circle({
+                x: x,
+                y: y,
+                stroke: '#666',
+                fill: '#ddd',
+                strokeWidth: 1,
+                radius: 8,
+                name: name,
+                draggable: true,
+                dragOnTop: false
+            });
+            
+            anchor.on('dragmove', (e) => {
+                this.update(e.target);
+                layer.draw();
+            });
+            anchor.on('mousedown touchstart', function() {
+                group.setDraggable(false);
+                this.moveToTop();
+            });
+            anchor.on('dragend', function() {
+                group.setDraggable(true);
+                layer.draw();
+            });
+            // add hover styling
+            anchor.on('mouseover', function() {
+                var layer = this.getLayer();
+                document.body.style.cursor = 'pointer';
+                this.setStrokeWidth(4);
+                layer.draw();
+            });
+            anchor.on('mouseout', function() {
+                var layer = this.getLayer();
+                document.body.style.cursor = 'default';
+                this.setStrokeWidth(2);
+                layer.draw();
+            });
+
+            group.add(anchor);
+        },
         loadImages() {
             var width = document.getElementById('canvas').clientWidth;
             var height = document.getElementById('canvas').clientHeight;
-            function update(activeAnchor) {
-                var group = activeAnchor.getParent();
-                var topLeft = group.get('.topLeft')[0];
-                var topRight = group.get('.topRight')[0];
-                var bottomRight = group.get('.bottomRight')[0];
-                var bottomLeft = group.get('.bottomLeft')[0];
-                var image = group.get('Image')[0];
-                var anchorX = activeAnchor.getX();
-                var anchorY = activeAnchor.getY();
-                // update anchor positions
-                switch (activeAnchor.getName()) {
-                    case 'topLeft':
-                        topRight.setY(anchorY);
-                        bottomLeft.setX(anchorX);
-                        break;
-                    case 'topRight':
-                        topLeft.setY(anchorY);
-                        bottomRight.setX(anchorX);
-                        break;
-                    case 'bottomRight':
-                        bottomLeft.setY(anchorY);
-                        topRight.setX(anchorX);
-                        break;
-                    case 'bottomLeft':
-                        bottomRight.setY(anchorY);
-                        topLeft.setX(anchorX);
-                        break;
-                }
-                image.position(topLeft.position());
-                var width = topRight.getX() - topLeft.getX();
-                var height = bottomLeft.getY() - topLeft.getY();
-                if(width && height) {
-                    image.width(width);
-                    image.height(height);
-                }
-            }
-            function addAnchor(group, x, y, name) {
-                var stage = group.getStage();
-                var layer = group.getLayer();
-                var anchor = new Konva.Circle({
-                    x: x,
-                    y: y,
-                    stroke: '#666',
-                    fill: '#ddd',
-                    strokeWidth: 2,
-                    radius: 8,
-                    name: name,
-                    draggable: true,
-                    dragOnTop: false
-                });
-                anchor.on('dragmove', function() {
-                    update(this);
-                    layer.draw();
-                });
-                anchor.on('mousedown touchstart', function() {
-                    group.setDraggable(false);
-                    this.moveToTop();
-                });
-                anchor.on('dragend', function() {
-                    group.setDraggable(true);
-                    layer.draw();
-                });
-                // add hover styling
-                anchor.on('mouseover', function() {
-                    var layer = this.getLayer();
-                    document.body.style.cursor = 'pointer';
-                    this.setStrokeWidth(4);
-                    layer.draw();
-                });
-                anchor.on('mouseout', function() {
-                    var layer = this.getLayer();
-                    document.body.style.cursor = 'default';
-                    this.setStrokeWidth(2);
-                    layer.draw();
-                });
-                group.add(anchor);
-            }
+
+
             var stage = new Konva.Stage({
                 container: 'images',
                 width: width,
@@ -128,10 +132,10 @@ export default {
             });
             layer.add(darthVaderGroup);
             darthVaderGroup.add(darthVaderImg);
-            addAnchor(darthVaderGroup, 0, 0, 'topLeft');
-            addAnchor(darthVaderGroup, 200, 0, 'topRight');
-            addAnchor(darthVaderGroup, 200, 138, 'bottomRight');
-            addAnchor(darthVaderGroup, 0, 138, 'bottomLeft');
+            this.addAnchor(darthVaderGroup, 0, 0, 'topLeft');
+            this.addAnchor(darthVaderGroup, 200, 0, 'topRight');
+            this.addAnchor(darthVaderGroup, 200, 138, 'bottomRight');
+            this.addAnchor(darthVaderGroup, 0, 138, 'bottomLeft');
             var yodaGroup = new Konva.Group({
                 x: 20,
                 y: 110,
@@ -139,10 +143,10 @@ export default {
             });
             layer.add(yodaGroup);
             yodaGroup.add(yodaImg);
-            addAnchor(yodaGroup, 0, 0, 'topLeft');
-            addAnchor(yodaGroup, 93, 0, 'topRight');
-            addAnchor(yodaGroup, 93, 104, 'bottomRight');
-            addAnchor(yodaGroup, 0, 104, 'bottomLeft');
+            this.addAnchor(yodaGroup, 0, 0, 'topLeft');
+            this.addAnchor(yodaGroup, 93, 0, 'topRight');
+            this.addAnchor(yodaGroup, 93, 104, 'bottomRight');
+            this.addAnchor(yodaGroup, 0, 104, 'bottomLeft');
             var imageObj1 = new Image();
             imageObj1.onload = function() {
                 darthVaderImg.image(imageObj1);
