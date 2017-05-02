c7328a726b92c7b9650cc5a6dd6b84644c5d38af
Katie Noland <katie@devetry.com>
2017-05-02 15:25:10 -0600

    load getters

--- src/store/modules/models/index.js
+++ src/store/modules/models/index.js
@@ -1,5 +1,6 @@
 import actions from './actions'
 import mutations from './mutations'
+import getters from './getters'
 
 export default {
     namespaced: true,
@@ -26,10 +27,6 @@ export default {
                 space_type_id: null,
                 construction_set_id: null
             }],
-            windows: [{
-                window_id: null,
-                vertex_id: null
-            }],
             shading: [{
                 name: null,
                 face_id: null
@@ -41,82 +38,16 @@ export default {
         }*/],
         // lib
         library: {
-            'building_units': [{
-                id: '1003',
-                name: 'Building Unit 1'
-            }, {
-                id: '1004',
-                name: 'Building Unit 2'
-            }, {
-                id: '1005',
-                name: 'Building Unit 3'
-            }],
-            'thermal_zones': [{
-                id: '1006',
-                name: 'Thermal Zone 1'
-            }, {
-                id: '1007',
-                name: 'Thermal Zone 2'
-            }, {
-                id: '1008',
-                name: 'Thermal Zone 3'
-            }],
-            'space_types': [{
-                id: '1009',
-                name: 'Space Type 1'
-            }, {
-                id: '1010',
-                name: 'Space Type 2'
-            }, {
-                id: '1011',
-                name: 'Space Type 3'
-            }, {
-                id: '1012',
-                name: 'Space Type 4'
-            }],
-            'construction_sets': [{
-                id: '1013',
-                name: 'Construction Set 1'
-            }, {
-                id: '1014',
-                name: 'Construction Set 2'
-            }],
-            'constructions': [{
-                id: '1015',
-                name: 'Construction 1'
-            }],
-            'windows': [{
-                id: '1016',
-                name: 'Window 1'
-            }, {
-                id: '1017',
-                name: 'Window 2'
-            }, {
-                id: '1021',
-                name: 'Window 3'
-            }],
-            'daylighting_controls': [{
-                id: '1018',
-                name: 'Daylighting Control 1'
-            }, {
-                id: '1019',
-                name: 'Daylighting Control 2'
-            }, {
-                id: '1020',
-                name: 'Daylighting Control 3'
-            }]
-
-
-            // building_units: [],
-            // thermal_zones: [],
-            // space_types: [],
-            // construction_sets: [],
-            // constructions: [],
-            // windows: [],
-            // daylighting_controls: []
+            building_units: [],
+            thermal_zones: [],
+            space_types: [],
+            construction_sets: [],
+            constructions: [],
+            windows: [],
+            daylighting_controls: []
         }
     },
     actions: actions,
     mutations: mutations,
-    getters: {}
+    getters: getters
 }
