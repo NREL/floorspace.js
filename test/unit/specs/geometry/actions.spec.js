7aab1c4d57600cdfa550223b6907b6cc67c4fede
Brian Schiller <bgschiller@gmail.com>
2017-08-18 12:54:11 -0600

    add test for that case

--- test/unit/specs/geometry/actions.spec.js
+++ test/unit/specs/geometry/actions.spec.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import { getOrCreateVertex } from '../../../../src/store/modules/geometry/actions/actions';
-import { assert } from '../../test_helpers';
+import { matchOrCreateEdges } from '../../../../src/store/modules/geometry/actions/createFaceFromPoints';
+import { assert, assertEqual } from '../../test_helpers';
 
 describe('getOrCreateVertex', () => {
   const geometry = {
@@ -14,5 +15,14 @@ describe('getOrCreateVertex', () => {
   it('will create a new vertex if necessary', () => {
     const newVert = getOrCreateVertex(geometry, { x: 1, y: 1 });
     assert(newVert.id !== 'already_extant');
+  });
+});
+
+
+describe('matchOrCreateEdges', () => {
+  const existingEdges = [{ id: 1, v1: 2, v2: 3 }, { id: 4, v1: 5, v2: 6 }];
+  it('can handle an empty list', () => {
+    const edges = matchOrCreateEdges([], existingEdges);
+    assertEqual(edges, []);
   });
 });
