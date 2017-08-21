6b21e7855ef94239df4c8a8561cd3cb86a9aa03f
Brian Schiller <bgschiller@gmail.com>
2017-08-21 16:43:20 -0600

    Add initial grid size and north axis

--- src/api.js
+++ src/api.js
@@ -35,6 +35,8 @@ window.api = {
       units: 'm',
       showMapDialogOnStart: false,
       online: true,
+      initialGridSize: 50,
+      initialNorthAxis: 0,
       onChange: () => { window.versionNumber += 1; },
     }, config);
   },
@@ -49,6 +51,8 @@ window.api = {
     // don't dispatch actions until the application and data store are instantiated
     window.application.$store.dispatch('project/setUnits', { units: window.api.config.units });
     window.application.$store.dispatch('project/setShowImportExport', window.api.config.showImportExport);
+    window.application.$store.dispatch('project/setSpacing', { spacing: window.api.config.initialGridSize });
+    window.application.$store.dispatch('project/setNorthAxis', { north_axis: window.api.config.initialNorthAxis });
 
     // if the map modal has been disabled, mark the map as initialized so that time travel can be initialized
     // TODO: we may want to intitialize timetravel in the importFloorplan action instead
