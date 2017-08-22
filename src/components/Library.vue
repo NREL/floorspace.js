b2ce8cae04b9240e3abea30ffba9e3d4066b93be
Brian Schiller <bgschiller@gmail.com>
2017-08-22 13:58:57 -0600

    dblclick only on slider bar

--- src/components/Library.vue
+++ src/components/Library.vue
@@ -7,9 +7,9 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER, THE UNITED STATES GOVERNMENT, OR ANY CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. -->
 
 <template>
-  <div id="layout-library" v-on:dblclick="showHide">
+  <div id="layout-library">
     <section id='library'>
-    <div @mousedown="resizeBarClicked" id="top-bar" ref="resizebar" class="resize-bar"></div>
+    <div @mousedown="resizeBarClicked" v-on:dblclick="showHide" id="top-bar" ref="resizebar" class="resize-bar"></div>
       <header>
           <div class="input-text">
               <label>Search</label>
