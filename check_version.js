b6747836b8a4df533f0b1395ffede97c32cb1e63
Brian Schiller <bgschiller@gmail.com>
2017-08-23 11:51:31 -0600

    fix check_version.js (and colors!)

--- check_version.js
+++ check_version.js
@@ -1,9 +1,20 @@
 const
-  pack = require('./package.json'),
-  schema = require('./schema/geometry_schema.json'),
+  pack = require('./package.json')['version'],
+  schema = require('./schema/geometry_schema.json')['version'],
   version = require('./src/version.js');
 
 if (pack !== schema || schema !== version) {
   console.error('version numbers in package.json, geometry_schema.json and version.js disagree!');
+  console.log('package.json:', pack);
+  console.log('geometry_schema.json:', schema);
+  console.log('version.js', version);
   process.exit(1);
 }
+
+function success(message) {
+  console.log('\x1b[32m' + message + '\x1b[0m');
+}
+
+success('package.json:         ' + pack);
+success('geometry_schema.json: ' + schema);
+success('version.js:           ' + version);
