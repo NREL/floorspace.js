e91e4ab1a0721ecaf9f81b1ed836be72baa8deb4
Brian Schiller <bgschiller@gmail.com>
2017-08-31 10:36:52 -0600

    working on nightwatch tests

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -1,19 +1,43 @@
 // For authoring Nightwatch tests, see
 // http://nightwatchjs.org/guide#usage
+const d3 = require('d3');
 
 module.exports = {
-  'default e2e tests': function (browser) {
-    // automatically uses dev Server port from /config.index.js
-    // default: http://localhost:8080
-    // see nightwatch.conf.js
-    const devServer = browser.globals.devServerURL
+  'make space and new story': (browser) => {
+    const devServer = browser.globals.devServerURL;
+
+    let yScale;
+    let xScale;
 
     browser
       .url(devServer)
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
-      .end()
-  }
-}
+      .waitForElementVisible('.modal .new-floorplan', 5000)
+      .click('.modal .new-floorplan')
+      .waitForElementVisible('#grid svg', 500)
+      .execute(
+        'return window.application.$store.state.application.scale', [],
+        ({ value: scale }) => {
+          console.log('scale is', JSON.stringify(scale));
+          xScale = d3.scaleLinear()
+            .domain(scale.x.rwuRange)
+            .range([0, scale.x.pixel]);
+          yScale = d3.scaleLinear()
+            .domain(scale.y.rwuRange)
+            .range([scale.y.pixel, 0]);
+        })
+      .perform((client, done) => {
+        client
+        .pause(100)
+        .moveTo('#grid svg', xScale(-50), yScale(0))
+        .pause(100)
+        .mouseButtonClick()
+        .pause(100)
+        .moveTo('#grid svg', xScale(0), yScale(50))
+        .pause(100)
+        .mouseButtonClick()
+        .waitForElementVisible('#grid svg g.poly', 200);
+        done();
+      })
+      .pause();
+  },
+};
