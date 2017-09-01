0de544a8d1800615dfcf1a83c24ea955bf6936ba
Brian Schiller <bgschiller@gmail.com>
2017-09-01 11:25:35 -0600

    add failing test

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -28,7 +28,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
                 {{displayNameForMode(mode)}}
             </button>
 
-            <button v-if="mode!=='images'" @click="createItem()">
+            <button v-if="mode!=='images'" @click="createItem()" class="add-sub-selection">
                 <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
                     <path d="M208 122h-74V48c0-3.534-2.466-6.4-6-6.4s-6 2.866-6 6.4v74H48c-3.534 0-6.4 2.466-6.4 6s2.866 6 6.4 6h74v74c0 3.534 2.466 6.4 6 6.4s6-2.866 6-6.4v-74h74c3.534 0 6.4-2.466 6.4-6s-2.866-6-6.4-6z"/>
                 </svg>
