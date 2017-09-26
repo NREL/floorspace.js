3da69ea8e863be4b206fb2c73303b2d0fef06018
Brian Schiller <bgschiller@gmail.com>
2017-09-26 12:26:38 -0600

    make tests less flaky

--- test/e2e/helpers.js
+++ test/e2e/helpers.js
@@ -63,7 +63,7 @@ function withScales(browser) {
   };
   return browser;
 }
-function drawSquare(x0, y0, width, height, options = { dontFinish: false }) {
+function drawSquare(x0, y0, width, height) {
   function draw(client, done) {
     console.log(`moving to ${client.xScale(x0)}, ${client.yScale(y0)}`);
     client
@@ -73,40 +73,17 @@ function drawSquare(x0, y0, width, height, options = { dontFinish: false }) {
     .pause(10)
     .mouseButtonClick()
     .pause(10)
-    .moveToElement('#grid svg', client.xScale(x0 + width), client.yScale(y0 + height));
-    if (!options.dontFinish) {
-      client.mouseButtonClick();
-    }
+    .moveToElement('#grid svg', client.xScale(x0 + width), client.yScale(y0 + height))
+    .mouseButtonClick();
+
     done();
   }
   return draw;
 }
 
-function draw50By50Square(client, done) {
-  client
-    .perform(drawSquare(-50, 0, 50, 50, { dontFinish: true }));
-  client.expect.element('.guideline-dist').text.to.contain('50');
-  client.expect.element('.guideline-area-text').text.to.contain('2,500');
-
-  client.mouseButtonClick()
-    .pause(10);
-
-  client.execute(
-    'return window.application.$store.state.geometry[0].vertices', [],
-    ({ value: verts }) => {
-      [{ x: -50, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 50 }, { x: -50, y: 50 }]
-      .forEach((vert) => {
-        client.assert.ok(_.find(verts, vert));
-      });
-    });
-
-  done();
-
-}
-
 module.exports = {
   failOnError: failOnError,
   withScales: withScales,
-  draw50By50Square: draw50By50Square,
+  draw50By50Square: drawSquare(-50, 0, 50, 50),
   drawSquare: drawSquare,
 };
