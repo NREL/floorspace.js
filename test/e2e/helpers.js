650a1c48eb6f6a45d4aabd91372525c4a58969dc
Brian Schiller <bgschiller@gmail.com>
2017-09-26 12:26:38 -0600

    make tests less flaky

--- test/e2e/helpers.js
+++ test/e2e/helpers.js
@@ -70,12 +70,10 @@ function drawSquare(x0, y0, width, height, options = { dontFinish: false }) {
     .waitForElementVisible('#grid svg', 200)
     .pause(10)
     .moveToElement('#grid svg', client.xScale(x0), client.yScale(y0))
-    .waitForElementVisible('#grid svg .gridpoint, #grid svg .highlight', 200)
     .pause(10)
     .mouseButtonClick()
     .pause(10)
-    .moveToElement('#grid svg', client.xScale(x0 + width), client.yScale(y0 + height))
-    .waitForElementVisible('.guideline-area-text', 100);
+    .moveToElement('#grid svg', client.xScale(x0 + width), client.yScale(y0 + height));
     if (!options.dontFinish) {
       client.mouseButtonClick();
     }
