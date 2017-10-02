14537cb1a77d509e74db6285597de40acfacc96e
Brian Schiller <bgschiller@gmail.com>
2017-10-02 12:15:05 -0600

    move svgs to one package

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -214,29 +214,9 @@ import { mapState } from 'vuex';
 import SaveAsModal from './Modals/SaveAsModal.vue';
 import AssignComponentMenu from './AssignComponentMenu.vue';
 import applicationHelpers from './../store/modules/application/helpers';
+import svgs from './svgs';
 
 // svgs
-import openFloorplanSvg from './../assets/svg-icons/open_floorplan.svg'
-import saveFloorplanSvg from './../assets/svg-icons/save_floorplan.svg'
-import importLibrarySvg from './../assets/svg-icons/import_library.svg'
-
-import undoSvg from './../assets/svg-icons/undo.svg'
-import redoSvg from './../assets/svg-icons/redo.svg'
-
-import floorplanTabSvg from './../assets/svg-icons/tab_floorplan.svg'
-import shadingTabSvg from './../assets/svg-icons/tab_shading.svg'
-import assignTabSvg from './../assets/svg-icons/tab_assign.svg'
-import componentsTabSvg from './../assets/svg-icons/tab_components.svg'
-
-import toolDrawRectangleSvg from './../assets/svg-icons/tool_draw_rectangle.svg'
-import toolDrawPolygonSvg from './../assets/svg-icons/tool_draw_polygon.svg'
-import toolEraseSvg from './../assets/svg-icons/tool_erase.svg'
-import toolMoveSizeSvg from './../assets/svg-icons/tool_move_size.svg'
-import toolColorSvg from './../assets/svg-icons/tool_color.svg'
-
-import zoomInSvg from './../assets/svg-icons/zoom_in.svg'
-import zoomOutSvg from './../assets/svg-icons/zoom_out.svg'
-import panSvg from './../assets/svg-icons/pan.svg'
 
 export default {
   name: 'toolbar',
@@ -412,25 +392,7 @@ export default {
   components: {
     'save-as-modal': SaveAsModal,
     'assign-component-menu': AssignComponentMenu,
-    'open-floorplan-svg': openFloorplanSvg,
-    'save-floorplan-svg': saveFloorplanSvg,
-    'import-library-svg': importLibrarySvg,
-    'undo-svg': undoSvg,
-    'redo-svg': redoSvg,
-    'tab-floorplan-svg': floorplanTabSvg,
-    'tab-shading-svg': shadingTabSvg,
-    'tab-assign-svg': assignTabSvg,
-    'tab-components-svg': componentsTabSvg,
-
-    'tool-color-svg': toolColorSvg,
-    'tool-draw-rectangle-svg': toolDrawRectangleSvg,
-    'tool-draw-polygon-svg': toolDrawPolygonSvg,
-    'tool-erase-svg': toolEraseSvg,
-    'tool-move-size-svg': toolMoveSizeSvg,
-
-    'zoom-in-svg': zoomInSvg,
-    'zoom-out-svg': zoomOutSvg,
-    'pan-svg': panSvg,
+    ...svgs,
   },
 };
 </script>
---
+++ src/components/svgs.js
@@ -0,0 +1,45 @@
+import openFloorplanSvg from './../assets/svg-icons/open_floorplan.svg';
+import saveFloorplanSvg from './../assets/svg-icons/save_floorplan.svg';
+import importLibrarySvg from './../assets/svg-icons/import_library.svg';
+
+import undoSvg from './../assets/svg-icons/undo.svg';
+import redoSvg from './../assets/svg-icons/redo.svg';
+
+import floorplanTabSvg from './../assets/svg-icons/tab_floorplan.svg';
+import shadingTabSvg from './../assets/svg-icons/tab_shading.svg';
+import assignTabSvg from './../assets/svg-icons/tab_assign.svg';
+import componentsTabSvg from './../assets/svg-icons/tab_components.svg';
+
+import toolDrawRectangleSvg from './../assets/svg-icons/tool_draw_rectangle.svg';
+import toolDrawPolygonSvg from './../assets/svg-icons/tool_draw_polygon.svg';
+import toolEraseSvg from './../assets/svg-icons/tool_erase.svg';
+import toolMoveSizeSvg from './../assets/svg-icons/tool_move_size.svg';
+import toolColorSvg from './../assets/svg-icons/tool_color.svg';
+
+import zoomInSvg from './../assets/svg-icons/zoom_in.svg';
+import zoomOutSvg from './../assets/svg-icons/zoom_out.svg';
+import panSvg from './../assets/svg-icons/pan.svg';
+import addImage from './../assets/svg-icons/add_image.svg';
+
+export default {
+  'open-floorplan-svg': openFloorplanSvg,
+  'save-floorplan-svg': saveFloorplanSvg,
+  'import-library-svg': importLibrarySvg,
+  'undo-svg': undoSvg,
+  'redo-svg': redoSvg,
+  'tab-floorplan-svg': floorplanTabSvg,
+  'tab-shading-svg': shadingTabSvg,
+  'tab-assign-svg': assignTabSvg,
+  'tab-components-svg': componentsTabSvg,
+
+  'tool-color-svg': toolColorSvg,
+  'tool-draw-rectangle-svg': toolDrawRectangleSvg,
+  'tool-draw-polygon-svg': toolDrawPolygonSvg,
+  'tool-erase-svg': toolEraseSvg,
+  'tool-move-size-svg': toolMoveSizeSvg,
+
+  'zoom-in-svg': zoomInSvg,
+  'zoom-out-svg': zoomOutSvg,
+  'pan-svg': panSvg,
+  'add-image': addImage,
+};
