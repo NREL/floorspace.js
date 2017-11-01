892b3ff8a9ebf7d670fd11fe7bccc97c74e49439
Brian Schiller <bgschiller@gmail.com>
2017-11-01 17:03:57 -0600

    move tooltips above icon

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -27,17 +27,31 @@ export default {
   .tooltip-text {
     visibility: hidden;
     width: 300px;
+    bottom: 100%;
+    left: 50%;
+    margin-left: -150px;
+    position: absolute;
+    z-index: 1;
+
     background-color: black;
     color: $gray-lightest;
     text-align: center;
     padding: 5px 5px;
     border-radius: 6px;
-
-    position: absolute;
-    z-index: 1;
   }
   &:hover .tooltip-text {
     visibility: visible;
   }
+  .tooltip-text::after {
+    content: " ";
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -9px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: black transparent transparent transparent;
+  }
+
 }
 </style>
