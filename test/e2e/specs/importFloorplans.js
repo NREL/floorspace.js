e35ed291c1cbe27bbe3524e7703fc800233ffabc
Brian Schiller <bgschiller@gmail.com>
2017-11-27 10:56:27 -0700

    fix indent

--- test/e2e/specs/importFloorplans.js
+++ test/e2e/specs/importFloorplans.js
@@ -14,22 +14,22 @@ module.exports = {
   },
   'import succeeds': (browser) => {
     browser
-    .setValue('#importInput', path.join(__dirname, '../fixtures/floorplan-2017-08-31.json'))
-    .waitForElementVisible('#grid svg polygon', 100)
-    .checkForErrors()
-    .end();
+      .setValue('#importInput', path.join(__dirname, '../fixtures/floorplan-2017-08-31.json'))
+      .waitForElementVisible('#grid svg polygon', 100)
+      .checkForErrors()
+      .end();
   },
   'project.north_axis new location': (browser) => {
     browser
-    .setValue('#importInput', path.join(__dirname, '../fixtures/floorplan-2017-08-31.json'))
-    .waitForElementVisible('#grid svg polygon', 100)
-    .execute('return window.application.$store.state.project.north_axis', [], ({ value }) => {
-      browser.assert.equal(value, 12, 'expected project.config.north_axis to be moved to project.north_axis');
-    })
-    .execute('return window.application.$store.state.project.config.north_axis', [], ({ value }) => {
-      browser.assert.ok(!value); // should be deleted from project.config
-    })
-    .checkForErrors()
-    .end();
+      .setValue('#importInput', path.join(__dirname, '../fixtures/floorplan-2017-08-31.json'))
+      .waitForElementVisible('#grid svg polygon', 100)
+      .execute('return window.application.$store.state.project.north_axis', [], ({ value }) => {
+        browser.assert.equal(value, 12, 'expected project.config.north_axis to be moved to project.north_axis');
+      })
+      .execute('return window.application.$store.state.project.config.north_axis', [], ({ value }) => {
+        browser.assert.ok(!value); // should be deleted from project.config
+      })
+      .checkForErrors()
+      .end();
   },
 };
