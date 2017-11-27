09c2bbc01d14ec5ed6a5b6798a092847c13bce18
Brian Schiller <bgschiller@gmail.com>
2017-11-27 14:14:28 -0700

    ground settings in expandable drawer, as per #249

---
+++ src/components/ExpandableDrawer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="expandable-drawer">
+    <p @click="expanded = !expanded">
+      {{ caret }} {{ title }}
+    </p>
+    <p v-if="expanded">
+      <slot />
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  props: ['title'],
+  data() {
+    return {
+      expanded: false,
+    }
+  },
+  computed: {
+    caret() {
+      return this.expanded ? '\u25BC' : '\u25BA';
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
