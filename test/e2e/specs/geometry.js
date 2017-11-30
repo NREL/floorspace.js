da6fd4448ef23bcb8cbc4451d8f0e5dee38b292e
Brian Schiller <bgschiller@gmail.com>
2017-11-30 10:51:41 -0700

    fix tests. nightwatch, I'm never using you again

--- test/e2e/specs/geometry.js
+++ test/e2e/specs/geometry.js
@@ -1,37 +1,36 @@
 const { failOnError, withScales, draw50By50Square, drawSquare } = require('../helpers');
 
+function start(browser) {
+  return failOnError(withScales(browser))
+    .url(browser.globals.devServerURL)
+    .resizeWindow(1000, 800)
+    .waitForElementVisible('.modal .new-floorplan', 5000)
+    .setFlagOnError()
+    .click('.modal .new-floorplan svg')
+    .getScales();
+}
+
+function finish(browser) {
+  browser
+    .checkForErrors()
+    .end();
+}
+
 module.exports = {
   tags: ['geometry'],
-  before: (browser) => {
-    failOnError(withScales(browser))
-      .url(browser.globals.devServerURL)
-      .resizeWindow(1000, 800);
-  },
-  beforeEach: (browser) => {
-    browser
-      .refresh()
-      .waitForElementVisible('.modal .new-floorplan', 5000)
-      .setFlagOnError()
-      .click('.modal .new-floorplan svg')
-      .getScales();
-  },
-  afterEach: (browser, done) => {
-    browser
-      .checkForErrors();
-    done();
-  },
-  after: (browser) => { browser.end(); },
   'drawing overlapping polygon should not duplicate vertices': (browser) => {
-    browser
+    start(browser)
       .perform(draw50By50Square)
       .click('[data-object-type="spaces"] .add-new')
       .perform(drawSquare(0, 50, 20, -20))
       .click('[data-object-type="spaces"] .rows > div:nth-child(1)')
       .perform(drawSquare(0, 50, 10, -30))
       .assert.validGeometry();
+    finish(browser);
   },
   'duplicate edge case (issue #253)': (browser) => {
-    browser.perform(draw50By50Square)
+    start(browser)
+      .perform(draw50By50Square)
       .click('[data-object-type="spaces"] .add-new')
       .perform(drawSquare(0, 65, 20, -25))
       .click('[data-object-type="spaces"] .add-new')
@@ -41,5 +40,6 @@ module.exports = {
       .click('[data-object-type="spaces"] .rows > div:nth-child(1)')
       .perform(drawSquare(0, 50, 10, -40))
       .assert.validGeometry();
+    finish(browser);
   },
 };
