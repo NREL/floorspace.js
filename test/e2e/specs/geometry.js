f76a197a12237294906dcf84a3ae3501c17f04f0
Brian Schiller <bgschiller@gmail.com>
2018-05-04 17:07:30 -0600

    fix test

--- test/e2e/specs/geometry.js
+++ test/e2e/specs/geometry.js
@@ -1,25 +1,19 @@
-const path = require('path');
-const { draw50By50Square, drawSquare, start, finish, failOnError, withScales } = require('../helpers');
+const { draw50By50Square, drawSquare, start, finish } = require('../helpers');
 
 module.exports = {
   tags: ['geometry'],
   'deformity that scott found (issue #301)': (browser) => {
-    failOnError(withScales(browser))
-      .url(browser.globals.devServerURL)
-      .resizeWindow(1000, 800)
-      .waitForElementVisible('.modal .new-floorplan', 5000)
-      .setFlagOnError()
-      .setValue('#importInput', path.join(__dirname, '../fixtures/deformity.json'))
-      .waitForElementVisible('#grid svg polygon', 100)
-      .getScales()
-      .click('[data-object-type="spaces"] .rows > div:nth-child(2)')
-      .perform(drawSquare(70, 60, 30, -15))
+    start(browser)
+      .perform(draw50By50Square)
+      .click('[data-object-type="spaces"] .add-new')
+      .perform(drawSquare(-40, 55, 30, -30))
+      .perform(drawSquare(-30, 25, 20, -10))
+      .perform(drawSquare(-30, 15, -10, 10))
       .execute(
-        "return _.map(application.$store.getters['geometry/denormalized'][0].faces, 'vertices')",
+        "return application.$store.getters['geometry/errors']",
         [],
         ({ value }) => {
-          const [verts1, verts2] = value;
-          browser.assert.ok(_.find(verts1, { x: 15, y: 45 }) && _.find(verts2, { x: 15, y: 45 }));
+          browser.assert.ok(value.length === 0);
         },
       );
     finish(browser);
