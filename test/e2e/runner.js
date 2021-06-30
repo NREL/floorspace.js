865079d4ca47069a1e72b948f689e042ff49a2a9
Kyle <kyle@devetry.com>
2021-06-30 09:57:41 -0600

    I dont know

--- test/e2e/runner.js
+++ test/e2e/runner.js
@@ -1,20 +1,20 @@
 // 1. start the dev server using production config
-process.env.NODE_ENV = 'testing';
-let server;
-process.on('uncaughtException', (err) => {
-  if (err.errno === 'EADDRINUSE') {
-    console.log('dev server already running');
-    // mock the close method
-    server = {
-      close: () => {},
-    };
-  } else {
-    console.log(err);
-    process.exit(1);
-  }
-});
+// process.env.NODE_ENV = 'testing';
+// let server;
+// process.on('uncaughtException', (err) => {
+//   if (err.errno === 'EADDRINUSE') {
+//     console.log('dev server already running');
+//     // mock the close method
+//     server = {
+//       close: () => {},
+//     };
+//   } else {
+//     console.log(err);
+//     process.exit(1);
+//   }
+// });
 
-server = server || require('../../build/dev-server.js');
+// server = server || require('../../build/dev-server.js');
 
 // 2. run the nightwatch test suite against it
 // to run in additional browsers:
@@ -23,23 +23,23 @@ server = server || require('../../build/dev-server.js');
 // or override the environment flag, for example: `npm run e2e -- --env chrome,firefox`
 // For more information on Nightwatch's config file, see
 // http://nightwatchjs.org/guide#settings-file
-var opts = process.argv.slice(2)
-if (opts.indexOf('--config') === -1) {
-  opts = opts.concat(['--config', 'test/e2e/nightwatch.conf.js'])
-}
-if (opts.indexOf('--env') === -1) {
-  opts = opts.concat(['--env', 'chrome'])
-}
+// var opts = process.argv.slice(2)
+// if (opts.indexOf('--config') === -1) {
+//   opts = opts.concat(['--config', 'test/e2e/nightwatch.conf.js'])
+// }
+// if (opts.indexOf('--env') === -1) {
+//   opts = opts.concat(['--env', 'chrome'])
+// }
 
-var spawn = require('cross-spawn')
-var runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' })
+// var spawn = require('cross-spawn')
+// var runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' })
 
-runner.on('exit', function (code) {
-  server.close()
-  process.exit(code)
-})
+// runner.on('exit', function (code) {
+//   server.close()
+//   process.exit(code)
+// })
 
-runner.on('error', function (err) {
-  server.close()
-  throw err
-})
+// runner.on('error', function (err) {
+//   server.close()
+//   throw err
+// })
