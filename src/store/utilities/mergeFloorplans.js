91b955ff25602db47552c893069079983bf4e32f
Kyle <kyle@devetry.com>
2021-08-10 13:47:45 -0600

    Making progress - current spaces not appearing though

--- src/store/utilities/mergeFloorplans.js
+++ src/store/utilities/mergeFloorplans.js
@@ -1,6 +1,25 @@
-// import _ from 'lodash';
+import _ from 'lodash';
+import idFactory from './generateId';
+import exportData from './export';
+import importFloorplan from './importFloorplan';
 
 export default function mergeFloorplans(context, payload) {
-  console.log('current state? ', context.state);
+  // export current floorplan
+  const currentFloorplan = exportData(context.state, context.getters);
+  console.log('current floorplan: ', currentFloorplan);
   console.log('payload contains new floorplan? ', payload);
+  // take in floorplan JSON to merge with current floorplan
+  const floorplanToMergeIn = payload.data;
+  // TODO: need way to iterate over both arrays
+  floorplanToMergeIn.stories[0].spaces = [...floorplanToMergeIn.stories[0].spaces, ...currentFloorplan.stories[0].spaces];
+  floorplanToMergeIn.stories[0].spaces = floorplanToMergeIn.stories[0].spaces.map((space, index) => ({
+    ...space,
+    id: idFactory.generate(),
+    name: `Space 1 - ${index}`,
+  }));
+  console.log('merged? ', floorplanToMergeIn.stories);
+  console.log('mutating payload? ', payload.data);
+  // merge the 2 objects together making sure to dedup ids
+  // import the new merged object
+  importFloorplan(context, payload);
 }
