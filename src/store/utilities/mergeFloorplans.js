27b42cf53d1e704eac12ff857fa0eabcc35d9059
Kyle <kyle@devetry.com>
2021-08-16 16:29:09 -0600

    Making progress on windows

--- src/store/utilities/mergeFloorplans.js
+++ src/store/utilities/mergeFloorplans.js
@@ -51,13 +51,27 @@ export default function mergeFloorplans(context, payload) {
       face_id: `l${space.face_id}`,
     }));
 
+    const windows = story.windows.map(window => ({
+      ...window,
+      edge_id: `l${window.edge_id}`,
+      id: `l${window.id}`,
+      window_definition_id: `l${window.window_definition_id}`,
+    }));
+
     return {
       ...story,
       id: `l${story.id}`,
       geometry,
       spaces,
+      windows,
     };
   });
+
+  // CURRENT WINDOW DEFS
+  currentFloorplan.window_definitions = currentFloorplan.window_definitions.map(window_def => ({
+    ...window_def,
+    id: `l${window_def.id}`,
+  }));
 
   // PREP IMPORTED FLOORPLAN FOR MERGE
   console.log('payload data: ', payload.data);
@@ -103,16 +117,31 @@ export default function mergeFloorplans(context, payload) {
       face_id: `r${space.face_id}`,
     }));
 
+    const windows = story.windows.map(window => ({
+      ...window,
+      edge_id: `r${window.edge_id}`,
+      id: `r${window.id}`,
+      window_definition_id: `r${window.window_definition_id}`,
+    }));
+
     return {
       ...story,
       id: `r${story.id}`,
       geometry,
       spaces,
+      windows,
     };
   });
 
+  // INCOMING WINDOW DEFS
+  payload.data.window_definitions = payload.data.window_definitions.map(window_def => ({
+    ...window_def,
+    id: `r${window_def.id}`,
+  }));
+
   // MERGE FLOORPLANS PROPERTIES
   const zipUpStories = _.zip(payload.data.stories, currentFloorplan.stories);
+  const zipUpWindowDefs = _.zip(payload.data.window_definitions, currentFloorplan.window_definitions).filter(x => x !== undefined);
   const mergeStories = zipUpStories.map((pairOfStories) => {
     if (!pairOfStories[0]) {
       return pairOfStories[1];
@@ -127,14 +156,26 @@ export default function mergeFloorplans(context, payload) {
         edges: [...pairOfStories[0].geometry.edges, ...pairOfStories[1].geometry.edges],
         faces: [...pairOfStories[0].geometry.faces, ...pairOfStories[1].geometry.faces],
         vertices: [...pairOfStories[0].geometry.vertices, ...pairOfStories[1].geometry.vertices],
+        windows: [...pairOfStories[0].windows, ...pairOfStories[1].windows],
       },
       spaces: [...pairOfStories[0].spaces, ...pairOfStories[1].spaces],
     };
   });
+  const mergeWindowDefs = zipUpWindowDefs.map((pairOfWindowDefs) => {
+    if (!pairOfWindowDefs[0]) {
+      return pairOfWindowDefs[1];
+    }
+    if (!pairOfWindowDefs[1]) {
+      return pairOfWindowDefs[0];
+    }
+    return [...pairOfWindowDefs[0].window_definitions, ...pairOfWindowDefs[1].window_definitions];
+  });
+
   const mergedResult = {
     data: {
       ...payload.data,
       stories: mergeStories,
+      window_definitions: mergeWindowDefs,
     },
   };
 
